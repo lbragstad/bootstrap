27f24201496a25d07c87ba81fbc4896c475e4c50
Lance Bragstad <lbragsta@redhat.com>
2022-09-23 15:52:27 -0500

    add nvim keymaps

---
+++ nvim/lua/lbragstad/keymaps.lua
@@ -0,0 +1,35 @@
+local opts = { noremap = true, silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+
+-- Simpler window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Open the navigation window
+keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+
+-- Navigate to next buffer
+keymap("n", "<leader>n", ":bn<CR>", opts)
+-- Navigate to previous buffer
+keymap("n", "<leader>p", ":bp<CR>", opts)
+
+keymap("n", "<leader>d", ":GoDoc<CR>", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize +2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize -2<CR>", opts)
+
+-- Quickly find files
+keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
