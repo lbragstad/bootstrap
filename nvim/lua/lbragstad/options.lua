39037b0a030fd6d71c4df3ddf25f5c8a5088892c
Lance Bragstad <lbragsta@redhat.com>
2022-09-23 15:44:35 -0500

    add basic nvim options

---
+++ nvim/lua/lbragstad/options.lua
@@ -0,0 +1,16 @@
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+vim.opt.autoread = false
+vim.opt.backup = false
+vim.opt.smartcase = true
+vim.opt.ruler = true
+vim.opt.cursorline = true
+vim.opt.ignorecase = true
+vim.opt.number = true
+vim.opt.expandtab = true
+vim.opt.scrolloff = 100
+vim.opt.cmdheight = 1
+vim.opt.background = 'dark'
+
+-- Treat hyphenated words as one word
+vim.cmd [[set iskeyword+=-]]
